24bd4b1ace3370fd982a0f8a875d4da7750f6c88
Tharinda-Pamindu <ict2022100@s.rjt.ac.lk>
2025-02-04 09:30:01 +0530

    subscribe.php added

--- DB.php
+++ DB.php
@@ -3,10 +3,13 @@ $conn = mysqli_connect("localhost","root","","venom_apparel");
 
 //Check Connection
 if(!$conn){
-    die("Connection failed : ". mysqli_connect_error());
+    die("Connection failed : ". mysqli_connect_error()); //when connection die this message occurred
+}else{
+    echo "Connection successful!<br>";
 }
 
 //Create user table
+// mysqli injection
 $userTableCreation = "CREATE TABLE IF NOT EXISTS user (
     ID INT AUTO_INCREMENT PRIMARY KEY,
     USERNAME VARCHAR(255) NOT NULL,
@@ -14,9 +17,25 @@ $userTableCreation = "CREATE TABLE IF NOT EXISTS user (
     PASSWORD VARCHAR(255) NOT NULL
 )";
 
-if(mysqli_query($conn, $userTableCreation)){
-    echo "Table created successfully";
+//subcribe table creation query
+$subscibeTableCreation = "CREATE TABLE IF NOT EXISTS subscribeLink (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE
+)";
+
+//user table sql code execute
+if(mysqli_query($conn, $userTableCreation)){ //in-built function in php
+    echo "Table created successfully <br>";
 }
 else{
     echo "<h3>500 - Internal server error<h3>";
 }
+
+if(mysqli_query($conn, $subscibeTableCreation)){
+    echo "Table created successfully <br>"; //print this if successfully created
+}else{
+    echo "<h3>500 - Internal server error<h3>";
+}
+
+$conn->close(); //connection close
+?>
--- subscribe.php
+++ subscribe.php
@@ -1,1 +1,30 @@
 <?php
+
+$conn = mysqli_connect("localhost", "root", "", "venom_apparel");
+if (!$conn) {
+    die("Connection failed : ". mysqli_connect_error());
+}
+$email = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['email'])) {
+        $email = $_POST['email'];
+    }
+
+    try {
+        $stmt = $conn->prepare("INSERT INTO subscribeLink (email) VALUES (?)");
+        $stmt->bind_param("s", $email);
+        if ($stmt->execute()) {
+            echo "New record created successfully!";
+        }
+        $stmt->close();
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
+} else {
+    echo "Error occurred: Invalid request method." . $_SERVER['REQUEST_METHOD'];
+}
+
+$conn->close();
+
+?>
